6116aec733cfda0f5e692c5749309a2b59faf3a8
bdin <b.din1996@gmail.com>
2016-07-14 14:13:23 -0700

    worked more on maps

--- src/js/searchable.js
+++ src/js/searchable.js
@@ -2,26 +2,60 @@ require("component-responsive-frame/child");
 require("angular");
 var app = angular.module("search", []);
 var inside = require('point-in-polygon');
+      
+var placeSearch, autocomplete;
+var latitude, longitude;
 
-for (var i = 0; i < neighborhoodData.features.length; i++) {
+function initAutocomplete() {
+	autocomplete = new google.maps.places.Autocomplete(
+	    (document.getElementById('autocomplete')),
+	      {
+	        types: ['geocode']
+	      }
+	    );
 
-  for (var h = 0; h < neighborhoodData.features[i].geometry.coordinates[0].length; h++)
+	autocomplete.addListener('place_changed', function(){
+        var geocoder = new google.maps.Geocoder();
+        var address = document.getElementById('autocomplete').value;
 
-  var polygon = neighborhoodData.features[i].geometry.coordinates[0][h];
-  var district = neighborhoodData.features[i].properties.neighbourhood;
-  var userinput = [120, 30];
+        geocoder.geocode({ 'address': address }, function (results, status) {
+          if (status == google.maps.GeocoderStatus.OK) {
+              latitude = results[0].geometry.location.lat();
+              longitude = results[0].geometry.location.lng();
+              console.log(longitude + ", " + latitude);
 
-  console.log(polygon);
+			for (var i = 0; i < neighborhoodData.features.length; i++) {
 
-  if (inside(userinput, polygon) == true) {
-    console.log("hello!");
-    console.log(district);
-  }
-  else {
-    console.log("fail");
-    console.log(district);
-  }
+			  for (var h = 0; h < neighborhoodData.features[i].geometry.coordinates[0].length; h++)
+
+			  var polygon = neighborhoodData.features[i].geometry.coordinates[0][h];
+			  var district = neighborhoodData.features[i].properties.neighbourhood;
+			  var userinput = [longitude, latitude];
+			  var yourneighborhood = "";
+			  var aboutyourarea = document.getElementById('aboutyourarea');
+
+			  console.log(district);
+
+			  if (inside(userinput, polygon) == true) {
+			    yourneighborhood = district;
+			    aboutyourarea.insertAdjacentHTML('afterbegin','<p>You live in ' + yourneighborhood + ".</p>");
+			    break;
+			  }
+			  else if ((inside(userinput,polygon) == false) && (i == neighborhoodData.features.length - 1)) {
+			    	aboutyourarea.insertAdjacentHTML('afterbegin',"<p>We couldn't locate your address.</p>");
+			  }
+			  else {
+
+			  }
+			}
+
+          }
+      })
+
+	})
 }
+
+initAutocomplete();
 
 app.controller("SearchController", ["$scope", function($scope) {
 
