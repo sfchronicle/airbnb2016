480b299e9fbb4b0ddfebd1506f195fdea2afe7f1
bdin <b.din1996@gmail.com>
2016-07-15 08:34:51 -0700

    updated database with combined files changed styles

--- src/js/searchable.js
+++ src/js/searchable.js
@@ -28,8 +28,8 @@ function initAutocomplete() {
 
 			  for (var h = 0; h < neighborhoodData.features[i].geometry.coordinates[0].length; h++)
 
-			  var polygon = neighborhoodData.features[i].geometry.coordinates[0][h];
-			  var district = neighborhoodData.features[i].properties.neighbourhood;
+			  var polygon = neighborhoodData.features[i].geometry.coordinates[0];
+			  var district = neighborhoodData.features[i].properties.name;
 			  var userinput = [longitude, latitude];
 			  var yourneighborhood = "";
 			  var aboutyourarea = document.getElementById('aboutyourarea');
@@ -37,15 +37,23 @@ function initAutocomplete() {
 			  console.log(district);
 
 			  if (inside(userinput, polygon) == true) {
+			  	aboutyourarea.innerHTML = '';
 			    yourneighborhood = district;
-			    aboutyourarea.insertAdjacentHTML('afterbegin','<p>You live in ' + yourneighborhood + ".</p>");
+			    aboutyourarea.insertAdjacentHTML('afterbegin','<p>You live in <span style="font-family: AntennaMedium">' + yourneighborhood + "</span>.</p>");
+
+			    if ((yourneighborhood == "Outer Mission") || (yourneighborhood == "West of Twin Peaks")) {
+			    	aboutyourarea.insertAdjacentHTML("beforeend","Data for your neighborhood is not available.");
+			    }
+			    else {
+			    	aboutyourarea.insertAdjacentHTML("beforeend","In " + yourneighborhood + ", there were a total of __ Airbnb listings.");
+			    }
 			    break;
 			  }
 			  else if ((inside(userinput,polygon) == false) && (i == neighborhoodData.features.length - 1)) {
+				  	aboutyourarea.innerHTML = '';
 			    	aboutyourarea.insertAdjacentHTML('afterbegin',"<p>We couldn't locate your address.</p>");
 			  }
 			  else {
-
 			  }
 			}
 
